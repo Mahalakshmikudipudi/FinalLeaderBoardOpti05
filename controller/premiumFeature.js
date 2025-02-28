034bc4dbd0a30ad08352f3af54cae678ef5021c0
Mahalakshmikudipudi <mahalakshmiguthula125@gmail.com>
2025-02-28 14:44:36 +0530

    LeaderBoardOptimisation

--- controller/premiumFeature.js
+++ controller/premiumFeature.js
@@ -5,20 +5,24 @@ const e = require('express');
 
 const getUserLeaderBoard = async (req, res) => {
     try{
-        const leaderboardofusers = await User.findAll({
-            attributes: ['id', 'name',[sequelize.fn('sum', sequelize.col('expenses.expenseamount')), 'total_cost'] ],
-            include: [
-                {
-                    model: Expense,
-                    attributes: []
-                }
-            ],
-            group:['user.id'],
-            order:[['total_cost', 'DESC']]
-
+        const users = await User.findAll()
+        const expenses = await Expense.findAll();
+        const userAggregatedExpenses = {}
+        console.log(expenses);
+        expenses.forEach((expense) => {
+            if(userAggregatedExpenses[expense.userId]) {
+                userAggregatedExpenses[expense.userId] = userAggregatedExpenses[expense.userId] + expense.expenseamount;
+            } else {
+                userAggregatedExpenses[expense.userId] = expense.expenseamount
+            }
+        });
+        var userLeaderBoardDetails = [];
+        users.forEach((user) => {
+            userLeaderBoardDetails.push({name: user.name, total_cost: userAggregatedExpenses[user.id] || 0})
         })
-       
-        res.status(200).json(leaderboardofusers)
+        console.log(userLeaderBoardDetails);
+        userLeaderBoardDetails.sort((a, b) => b.total_cost - a.total_cost);
+        res.status(200).json(expenses);
     
 } catch (err){
     console.log(err)
